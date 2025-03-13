58e8f51f396359e9b24b3c64ac6cbe4d20e50083
angel <mostazzita@proton.me>
2025-03-13 17:29:15 -0400

    ejercicio para convertir la temperatura en grados celsius a fahrenheit, aprobado

--- temperature-conversion/temperature-conversion.c
+++ temperature-conversion/temperature-conversion.c
@@ -2,7 +2,16 @@
 
 int main() {
 
+    int celsius;
+    int fahrenheit;
+    int temperature;
 
+    printf("Ingrese la temperatura: ");
+    scanf("%d", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("La temperatura ingresada en celsius, equivale a %d grados fahrenheit", fahrenheit);
 
     return 0;
 }
